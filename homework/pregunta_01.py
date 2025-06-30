b5ab44d509d9a5327016c47ae9fda220874215e7
Tomas Carmona Salazar <tcarmona@unal.edu.co>
2025-06-29 23:33:43 -0500

    Lab 4

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -1,73 +1,57 @@
 # pylint: disable=import-outside-toplevel
 # pylint: disable=line-too-long
 # flake8: noqa
+import os
+import pandas as pd
+import zipfile
 """
 Escriba el codigo que ejecute la accion solicitada en cada pregunta.
 """
 
+def generate_csv(ruta, target):
+    texts = []
+    # Recorrer todos los archivos en la carpeta
+    for file_name in os.listdir(ruta):
+            file_path = os.path.join(ruta, file_name)
+            
+            with open(file_path, "r", encoding="utf-8") as file:
+                content = file.read()
+                texts.append(content)
+            
+    df = pd.DataFrame([[text, target] for text in texts], columns=["phrase", "target"])
+    return df
+
+
+import zipfile
 
 def pregunta_01():
-    """
-    La información requerida para este laboratio esta almacenada en el
-    archivo "files/input.zip" ubicado en la carpeta raíz.
-    Descomprima este archivo.
+    # Descomprimir ZIP
+    with zipfile.ZipFile("files/input.zip", "r") as zip_ref:
+        zip_ref.extractall("files/")
 
-    Como resultado se creara la carpeta "input" en la raiz del
-    repositorio, la cual contiene la siguiente estructura de archivos:
+    targets = ["negative", "neutral", "positive"]
+    csvs_train = []
 
+    for target in targets:
+        csvs_train.append(generate_csv("files/input/train/" + target, target))
 
-    ```
-    train/
-        negative/
-            0000.txt
-            0001.txt
-            ...
-        positive/
-            0000.txt
-            0001.txt
-            ...
-        neutral/
-            0000.txt
-            0001.txt
-            ...
-    test/
-        negative/
-            0000.txt
-            0001.txt
-            ...
-        positive/
-            0000.txt
-            0001.txt
-            ...
-        neutral/
-            0000.txt
-            0001.txt
-            ...
-    ```
+    train_csv = pd.concat(csvs_train, ignore_index=True)
 
-    A partir de esta informacion escriba el código que permita generar
-    dos archivos llamados "train_dataset.csv" y "test_dataset.csv". Estos
-    archivos deben estar ubicados en la carpeta "output" ubicada en la raiz
-    del repositorio.
+    csvs_test = []
+    for target in targets:
+        csvs_test.append(generate_csv("files/input/test/" + target, target))
 
-    Estos archivos deben tener la siguiente estructura:
+    test_csv = pd.concat(csvs_test, ignore_index=True)
 
-    * phrase: Texto de la frase. hay una frase por cada archivo de texto.
-    * sentiment: Sentimiento de la frase. Puede ser "positive", "negative"
-      o "neutral". Este corresponde al nombre del directorio donde se
-      encuentra ubicado el archivo.
+    os.makedirs("files/output", exist_ok=True)
+    train_csv.to_csv("files/output/train_dataset.csv", index=False, encoding="utf-8")
+    test_csv.to_csv("files/output/test_dataset.csv", index=False, encoding="utf-8")
 
-    Cada archivo tendria una estructura similar a la siguiente:
-
-    ```
-    |    | phrase                                                                                                                                                                 | target   |
-    |---:|:-----------------------------------------------------------------------------------------------------------------------------------------------------------------------|:---------|
-    |  0 | Cardona slowed her vehicle , turned around and returned to the intersection , where she called 911                                                                     | neutral  |
-    |  1 | Market data and analytics are derived from primary and secondary research                                                                                              | neutral  |
-    |  2 | Exel is headquartered in Mantyharju in Finland                                                                                                                         | neutral  |
-    |  3 | Both operating profit and net sales for the three-month period increased , respectively from EUR16 .0 m and EUR139m , as compared to the corresponding quarter in 2006 | positive |
-    |  4 | Tampere Science Parks is a Finnish company that owns , leases and builds office properties and it specialises in facilities for technology-oriented businesses         | neutral  |
-    ```
+    print("CSV guardados correctamente.")
 
 
-    """
+   
+         
+
+
+pregunta_01()
